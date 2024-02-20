f629ab6e8d3f530dc34f7c77dd4c0a900ab2673d
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2024-02-20 10:43:12 -0500

    Delete barra home directory

--- barra home/assent/js/app/loginCheck.js
+++
@@ -1,8 +0,0 @@
-const overlay = document.getElementById("overlay");
-export const loginCheck = (user) => {
-  if (user) {
-    overlay.style.display = "none";
-  } else {
-    overlay.style.display = "block";
-  }
-};
